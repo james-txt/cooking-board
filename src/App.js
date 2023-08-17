1ff2d10e475ee5a88c0e1aee0bd5f376048dfbf2
James Lo <jameslo.txt@gmail.com>
2023-08-17 19:37:01 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,52 +1,38 @@
-import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Recipe from './components/Recipe';
-import Footer from './components/Footer';
-import Contact from './components/Contact';
-import About from './components/About';
-import { MsalProvider } from '@azure/msal-react';
-import { PublicClientApplication } from '@azure/msal-browser';
-import Config from './components/Config';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Helmet } from "react-helmet";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Recipe from "./components/Recipe";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import { Routes, Route } from "react-router-dom";
 
-const msalInstance = new PublicClientApplication(Config);
 
 function App() {
-  useEffect(() => {
-    // Listen for token message from popup
-    const handleMessage = (event) => {
-      if (event.origin === window.location.origin) {
-        // Handle the token here, e.g., store it in state or perform other actions
-        console.log('Received token:', event.data);
-      }
-    };
-    window.addEventListener('message', handleMessage);
-
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    };
-  }, []);
-
   return (
-    <MsalProvider instance={msalInstance}>
-      <div className="App">
-        <Helmet>
-          {/* Your Helmet content */}
-        </Helmet>
-        <header>
-          <Navbar />
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipe/:id" element={<Recipe />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />
-      </div>
-    </MsalProvider>
+    <div className="App">
+      <Helmet>
+        <html lang="en" dir="ltr"/>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/flowbite.min.css" rel="stylesheet"/>
+        <link href="./style.css" rel="stylesheet"/>
+        <script src="https://alcdn.msauth.net/browser/2.17.0/js/msal-browser.js"></script>
+        <title>Cooking Board</title>
+      </Helmet>
+      <header>
+        <Navbar/>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/recipe/:id" element={<Recipe/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+      <Footer/>
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,39 @@
 import React from "react";
 import logo from './img/logo-sm1.png';
-import { useMsal } from '@azure/msal-react';
-import { useNavigate } from 'react-router-dom'; // Import useHistory
-
-function Navbar() {
-  const { instance } = useMsal();
-  const navigate = useNavigate(); // Initialize useHistory
-
-  const handleSignIn = async () => {
-    try {
-      const response = await instance.loginPopup();
-      // Handle successful login
-
-      // Close the pop-up window
-      window.close();
-
-      // Send token to parent window
-      if (window.opener) {
-        window.opener.postMessage(response.accessToken, window.location.origin);
-      }
-      // Redirect to the homepage with the token
-      navigate('/', { token: response.accessToken });
-    } catch (error) {
-      console.error('Login error:', error);
-    }
-  };
+/*import { PublicClientApplication } from '@azure/msal-browser';*/
 
 
-    return (
-      <header className="sticky top-0 w-full z-auto">
-        <nav className="flex justify-between bg-stone-700 shadow-md shadow-stone-900 py-1.5 rounded-b-md">
-          <div id="logo" className="flex flex-shrink-0 items-center sm:ml-4 ml-2 z-0">
-            <a href="/">
-              <img src={logo} style={{ height: '50px', width: '52px' }} alt="Cooking Board Logo" loading="lazy" />
-            </a>
-            <span className="block text-stone-50 self-center sm:pl-4 pl-2 sm:text-4xl text-2xl font-medium whitespace-nowrap">COOKING BOARD</span>
-          </div>
-          <div className="flex right-0 md:mr-4 mr-2 items-center">
-            <button id="login" onClick={handleSignIn} className="bg-stone-200 rounded-full focus:ring-2 focus:ring-stone-900">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-8 h-8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-            </button>
-          </div>
-        </nav>
-      </header>
-    );
-  }
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 w-full z-auto">
+      <nav className="flex justify-between bg-stone-700 shadow-md shadow-stone-900 py-1.5 rounded-b-md">
+        <div id="logo" className="flex flex-shrink-0 items-center sm:ml-4 ml-2 z-0">
+          <a href="/">
+            <img src={logo} style={{ height: '50px', width: '52px' }} alt="Cooking Board Logo" loading="lazy" />
+          </a>
+          <span className="block text-stone-50 self-center sm:pl-4 pl-2 sm:text-4xl text-2xl font-medium whitespace-nowrap">COOKING BOARD</span>
+        </div>
+        <div className="flex right-0 items-center">
+          <button id="login" className="sm:mr-4 mx-1 bg-stone-200 rounded-full focus:ring-2 focus:ring-stone-900">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </button>
+        </div>
+   	 </nav>
+  	</header>
+	)
+}
 
-export default Navbar;
+export default Navbar
