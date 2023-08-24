897989e601efc270ec9f5ae59e4295e2ad81cc10
James Lo <jameslo.txt@gmail.com>
2023-08-24 14:42:03 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/flowbite.min.css" rel="stylesheet"/>
-        <link href="./style.css" rel="stylesheet"/>
         <script src="https://alcdn.msauth.net/browser/2.17.0/js/msal-browser.js"></script>
         <title>Cooking Board</title>
       </Helmet>
