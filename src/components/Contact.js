5e6023fe0f4819abcf8c7cf6532e2272cee8da3b
james-txt <jameslo.txt@gmail.com>
2023-09-05 23:03:38 -0400

    Merge branch 'main' of https://github.com/james-txt/cooking-board

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 
@@ -15,3 +16,22 @@ const Contact = () => {
   
 export default Contact
 
+=======
+import React from "react";
+
+
+const Contact = () => {
+  return (
+    <section className="flex justify-center">  
+      <div className="h-min w-[480px] mt-24 mb-12 text-md text-center rounded overflow-hidden shadow-md bg-white text-stone-800">
+        <div className="w-full max-w-screen-xl mx-auto p-6">
+          <p>Contact me: jameslo.txt@gmail.com</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+  
+export default Contact
+
+>>>>>>> 7b17c0b9019b76ccfe9c3cf0c2737ee46c853661
