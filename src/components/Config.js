13b204a04ea446779b4790902e1256b9d96cfcd0
james-txt <jameslo.txt@gmail.com>
2023-09-06 02:49:38 -0400

    test commit

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Config = {
   auth: {
     clientId: '0c562dfa-0a4e-400e-ae9b-8fea19c5e0a1',
@@ -12,18 +11,4 @@ const Config = {
 };
 
 export default Config;
-=======
-const Config = {
-  auth: {
-    clientId: '0c562dfa-0a4e-400e-ae9b-8fea19c5e0a1',
-    authority: 'https://login.microsoftonline.com/a408f434-d291-498e-943b-8ee3be1b817f',
-    redirectUri: 'https://www.cookingboard.live/',
-  },
-  cache: {
-    cacheLocation: 'sessionStorage',
-    storeAuthStateInCookie: false,
-  },
-};
 
-export default Config;
->>>>>>> 7b17c0b9019b76ccfe9c3cf0c2737ee46c853661
