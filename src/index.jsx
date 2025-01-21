d7cfe6925a825935c7273229eda7c6ab855388be
james-txt <jameslo.txt@gmail.com>
2025-01-21 13:35:52 -0500

    upd analytics integration

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
